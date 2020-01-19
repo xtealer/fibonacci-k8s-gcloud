f70cf07ed6dadc8cd41dd80d31b9586e1501219e
Enrique Shunnar <xtealer00@gmail.com>
2020-01-19 16:31:20 -0500

    deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+# build images
+docker build -t xtealer/fibonacci-k8s:client -t xtealer/fibonacci-k8s:client-$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t xtealer/fibonacci-k8s:server -t xtealer/fibonacci-k8s:server-$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t xtealer/fibonacci-k8s:worker -t xtealer/fibonacci-k8s:worker-$GIT_SHA -f ./worker/Dockerfile ./worker
+
+# push client image
+docker push xtealer/fibonacci-k8s:client
+docker push xtealer/fibonacci-k8s:client-$GIT_SHA
+# push server image
+docker push xtealer/fibonacci-k8s:server
+docker push xtealer/fibonacci-k8s:server-$GIT_SHA
+# push worker image
+docker push xtealer/fibonacci-k8s:worker
+docker push xtealer/fibonacci-k8s:worker-$GIT_SHA
+
+# apply configuration
+kubectl apply -f k8s
+
+# set new images for deployments
+kubectl set image deployments/client-deployment client=xtealer/fibonacci-k8s:client-$GIT_SHA
+kubectl set image deployments/server-deployment client=xtealer/fibonacci-k8s:server-$GIT_SHA
+kubectl set image deployments/worker-deployment client=xtealer/fibonacci-k8s:worker-$GIT_SHA
